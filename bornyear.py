f0e8e8c872c07e06ec7d99692d8403e2d0beb7cd
Jack <dev@adwt.ru>
2022-11-07 22:30:50 +0300

    Добален код в модуль bornday.py

--- bornyear.py
+++ bornyear.py
@@ -1,8 +1,13 @@
 # МОДУЛЬ 2
 # bornyear.py
-right_pushkin_birthday = 1799
-input_pushkin_birthday = int(input('Введите год рождения Саши Пушкина?: '))
-if input_pushkin_birthday != right_pushkin_birthday:
-  print('Неверно!')
-else:
-  print('Верно!')
+def bornyear():
+  right_pushkin_birthday = 1799
+  input_pushkin_birthday = int(input('Введите год рождения Саши Пушкина?: '))
+  if input_pushkin_birthday != right_pushkin_birthday:
+    print('Неверно!')
+  else:
+    print('Верно!')
+  pass
+
+if __name__ == '__main__':
+  bornyear()
