befd1e1ad424d14787a590c4e8cfa49e8ce8b12d
Jack <dev@adwt.ru>
2022-11-07 22:19:00 +0300

    Добален код в модуль bornear.py

---
+++ bornyear.py
@@ -0,0 +1,8 @@
+# МОДУЛЬ 2
+# bornyear.py
+right_pushkin_birthday = 1799
+input_pushkin_birthday = int(input('Введите год рождения Саши Пушкина?: '))
+if input_pushkin_birthday != right_pushkin_birthday:
+  print('Неверно!')
+else:
+  print('Верно!')
